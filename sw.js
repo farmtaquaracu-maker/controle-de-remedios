92cde841b537982101dd2508ea12e730010a6337
farmtaquaracu-maker <farmtaquaracu@gmail.com>
2026-09-24 22:28:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
-const CACHE='remedios-v3';
-const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
+// SW v5 - apaga cache antigo
+self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/'))));
+  e.respondWith(fetch(e.request));
 });
